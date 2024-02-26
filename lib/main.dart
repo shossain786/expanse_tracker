b5d3f93e94f2b447fcac3b73f137c0a84bb75ae4
Saddam <sddmhossain@786gmail.com>
2024-02-26 23:45:41 +0530

    adding code

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:expanse_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(useMaterial3: true),
-    home: const Expenses(),
-  ));
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const Expenses(),
+    ),
+  );
 }
